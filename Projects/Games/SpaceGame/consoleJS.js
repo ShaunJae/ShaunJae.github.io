96647f1f4f9128a7d6b633900be8feae5752522f
Shaun <32381182+ShaunJae@users.noreply.github.com>
2022-04-26 23:19:38 -0400

    Add files via upload

---
+++ Projects/Games/SpaceGame/consoleJS.js
@@ -0,0 +1,26 @@
+var commands = {
+	"ship": {
+		"rename": renameShip
+	}
+}
+
+function renameShip(string){
+	console.log(string);
+}
+
+function consoleSubmit(){
+	var input = document.getElementById("consoleInputField").value;
+}
+
+function parseCommand(command){
+	var parts = command.split(" ");
+	var partsLength = parts.length;
+	var baseCommands = Object.keys(commands);
+	if(partsLength > 1 && baseCommands.includes(parts[0])){
+		var commandCommands = Object.keys(commands[parts[0]]);
+		if(partsLength > 2 && commandCommands.includes(parts[1])){
+			commands[parts[0]][parts[1]]("Parsing successful");
+		}
+	}
+}
+commands["ship"]["rename"]("hi!")
---
+++ Projects/Games/SpaceGame/missionJS.js
@@ -0,0 +1,14 @@
+var s = document.createElement("select");
+
+function createOption(t, i){
+	var o = document.createElement("option");
+	o.innerHTML = t;
+	o.setAttribute("value", i)
+	return o;
+}
+
+for(var i = 1; i < Object.keys(ships).length+1; i++){
+	s.append(createOption("Ship " + String(i), i));
+}
+var hangar = document.getElementById("currentTrade");
+hangar.append(s);
